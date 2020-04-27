511ec98c910a18f21421b1d80abf8097e3feedeb
Alessandro <alessandro.angioi@hotmail.com>
2020-04-27 22:00:02 +0200

    Add test_ebooks.py

---
+++ test/test_ebooks.py
@@ -0,0 +1,6 @@
+import pytest
+
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# TODO: Write tests. Sample book @ data/Ye_olde_epub_teste.epub
+# Especially important: Loading a book's body
