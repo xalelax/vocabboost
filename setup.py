460d0b0a44ae307375674fe9a3716d511148f9ba
Alessandro <alessandro.angioi@hotmail.com>
2020-04-27 21:12:42 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vocabboost',
+    version='0.1',
+    description='Characteristic words definitions from ebooks',
+    author='Alessandro Angioi',
+    author_email='alessandro.angioi@hotmail.com',
+    packages=find_packages()
+)
